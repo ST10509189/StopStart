a9bb59723426eb6d09132643e4eafac5c024677a
ST10509189 <zilorgat2@gmail.com>
2026-02-25 21:55:58 +0200

    Final submission

---
+++ src/main/java/com/mycompany/startstop/StartStop.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.startstop;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StartStop {
+
+    public static void main(String[] args) {
+        System.out.println("                    ***********                   ");
+        System.out.println("       ******                      ******"         ); 
+        System.out.println("      *      *                    *       *       ");
+        System.out.println("     *   go   *                  *   stop  *      ");
+        System.out.println("      *      *                    *       *       ");
+        System.out.println("       ******                      *******        ");
+        System.out.println("         **                          ***          ");
+        System.out.println("         **                          ***          ");
+        System.out.println("         **                          ***          ");
+        System.out.println("         **                          ***          ");
+      
+        
+     
+        
+
+        
+
+    }
+}
